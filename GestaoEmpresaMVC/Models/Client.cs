4771938802063baa2e80f09fc83a1b0ee90d0f57
Otavio Rockenbach <otavio.atr@gmail.com>
2020-05-29 17:23:12 -0300

    Client Class & BuscaCEP.js Add

---
+++ GestaoEmpresaMVC/Models/Client.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GestaoEmpresaMVC.Models
+{
+    public class Client
+    {
+        public int Id { get; set; }
+        public int Cpf { get; set; }
+        public string Name { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string Gender { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public int SalesRecord { get; set; }
+
+        //Propriedades de Endereco
+        //Enderecço vem de outra classe pra utilizar a API? ou várias propriedades? Estado, Cidade, Rua....
+        public int Cep { get; set; }
+        public string State { get; set; }
+        public string City { get; set; }
+        public string District { get; set; }
+        public string Address { get; set; }
+        public int Number { get; set; }
+        public string Complement { get; set; }
+
+    }
+}
---
+++ GestaoEmpresaMVC/wwwroot/lib/jquery/dist/BuscaCep.js
@@ -0,0 +1,18 @@
+﻿jQuery(function ($){
+    $("input[name='cep']").change(function () {
+        var cep_code = $(this).val();
+        if (cep_code.length <= 0) return;
+        $.get("http://apps.widenet.com.br/busca-cep/api/cep.json", { code: cep_code }, function (result) {
+            if (result.status != 1) {
+                alert(result.message || "Ops! Nós não conseguimos encontrar este CEP.");
+                return;
+            }
+            $("input[name='cep']").val(result.code);
+            $("input[name='estado']").val(result.state);
+            $("input[name='cidade']").val(result.city);
+            $("input[name='bairro']").val(result.district);
+            $("input[name='logradouro']").val(result.adress);
+            $("input[name='estado']").val(result.state);
+        });
+    });
+});
